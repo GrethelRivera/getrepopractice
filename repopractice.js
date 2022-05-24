ee463c954b110f546fade96f5267b03fb48fe71c
Rivera <GRivera@nfpa.org>
2022-05-24 19:30:00 -0400

    first commit

--- repopractice.js
+++ repopractice.js
@@ -1,5 +1,5 @@
 console.log('Hello')
-console.log('Welcome to the practice workbook')
-console.log('git add - fileName')
+console.log('Welcome to my repository practice workbook')
+console.log('Add Changes: git add - fileName')
 console.log('Save Changes: git commit -m "first commit"')
 console.log('Push Changes: git push -u origin main ')
